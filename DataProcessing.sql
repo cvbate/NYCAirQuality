1584f6e6b51347064a2f829b8e93e6ad58feb053
cvbate <cvalentinebate@gmail.com>
2024-04-12 20:45:01 +0000

    dataprocessing/setup

---
+++ DataProcessing.sql
@@ -0,0 +1,9 @@
+-- Connect to database in SQL Shell
+CREATE DATABASE "NYCAirQuality";
+
+\c NYCAirQuality
+
+CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS postgis_raster;
+------------------------------------------------------------------------
+-- Google Cloud Console
