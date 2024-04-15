4b969c60b7ba2c9ae2ac3eae71922df5db60cb2d
cvbate <cbate@clarku.edu>
2024-04-15 11:38:23 -0400

    dataprocessing.sql

--- DataProcessing.sql
+++ DataProcessing.sql
@@ -14,8 +14,11 @@ CREATE EXTENSION IF NOT EXISTS postgis_raster;
 cd NYCAirQuality/DATA_REPROJECTED
 
 shp2pgsql -s 4326 -I buroughbounds.shp public.buroughbounds > buroughbounds.sql
+shp2pgsql -s 4326 -I cencustracts.shp public.cencustracts > cencustracts.sql
+shp2pgsql -s 4326 -I neighborshoods.shp public.neighborshoods > neighborshoods.sql
+shp2pgsql -s 4326 -I parks.shp public.parks > parks.sql
 
--- Raster
+-- Raster/ One nyc bounds shp
 -- for connecting to cloud storage to convert files to .sql
 -- first navigate to the folder you want to hold your data!
 -- cvalentinebate@cloudshell:~/rast (nycairquality)$ 
@@ -41,5 +44,14 @@ Copying gs://gee_data_nyc/aerosol_durr.tif...
  
 
 raster2pgsql -s 4326 -I -C -M aerosol_durr.tif public.aerosol_durr_rast > aerosol_durr.sql
+raster2pgsql -s 4326 -I -C -M aerosol_post.tif public.aerosol_post_rast > aerosol_post.sql
+raster2pgsql -s 4326 -I -C -M aerosol_pre.tif public.aerosol_pre_rast > aerosol_pre.sql
+
+raster2pgsql -s 4326 -I -C -M co_durr.tif public.co_durr_rast > co_durr.sql
+raster2pgsql -s 4326 -I -C -M co_post.tif public.co_post_rast > co_post.sql
+raster2pgsql -s 4326 -I -C -M co_pre.tif public.co_pre_rast > co_pre.sql
+
+raster2pgsql -s 4326 -I -C -M evi_nyc.tif public.evi_nyc_rast > evi_nyc.sql
+
 -- Processing 1/1: aerosol_durr.tif
 
